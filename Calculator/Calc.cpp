f9cceee8cbc87f8c4465dee8b50c6f328f02922d
Marci <128537619+marci1175@users.noreply.github.com>
2023-04-19 16:08:41 +0200

    Updated strings

--- Calculator/Calc.cpp
+++ Calculator/Calc.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 #include <stdlib.h>
 #include <Windows.h>
-std::string dontes;
+int dontes;
 int dontes1;
 int dontes2;
 int answ;
 bool resti = true;
 std::string fasz;
+template<> class std::numeric_limits<int>;
 void inv() {
-    system("CLS");
     std::cout << "\nThis number is bigger than the 32bit integer, therefor i wont calculate that!\nExiting in 10 seconds\n";
     Sleep(10000);
     resti = false;
@@ -18,47 +18,63 @@ int main()
     while(resti == true){
         std::cout << "Welcome to Marci's calculator\n1) +\n2) -\n3) :\n4) *\n";
         std::cin >> dontes;
-        std::cout << "User's decision : \n" << dontes;
-        std::cout << "\nEnter the first number!\n";
-        std::cin >> dontes1;
-        std::cout << "\nEnter the second number!\n";
-        std::cin >> dontes2;
-
-
-        if (dontes == "1") {
-            if (2147483647 <= dontes1 + dontes2 or -2147483639 >= dontes1 + dontes2) {
-                inv();
-            }
-            else
-                answ = dontes1 + dontes2;
-        }
-
-        else if (dontes == "2") {
-            if (2147483647 <= dontes1 - dontes2 or -2147483639 >= dontes1 - dontes2) {
-                inv();
-            }
-            else
-                answ = dontes1 - dontes2;
-        }
-        else if (dontes == "3") {
-            if (2147483647 <= dontes1 / dontes2 or -2147483639 >= dontes1 / dontes2) {
-                inv();
-            }
-            else
-                answ = dontes1 / dontes2;
-        }
-        else if (dontes == "4") {
-            if (2147483647 <= dontes1 * dontes2 or -2147483639 >= dontes1 * dontes2) {
-                inv();
-            }
-            else
-                answ = dontes1 * dontes2;
-        }
-        else
+        if (dontes > 4) {
             while (true) {
                 std::cout << "I hope you die in your sleep\n";
             }
 
+
+        }
+        
+        std::cout << "User's decision : \n" << dontes;
+        std::cout << "\nEnter the first number!\n";
+        try 
+        {
+        std::cin >> dontes1;
+        std::cout << "\nEnter the second number!\n";
+        std::cin >> dontes2;
+        }
+        catch (std::runtime_error& e) {
+            std::cout << "you have entered an invalid number/character, restarting in 10 seconds";
+                inv();
+        }
+        if (dontes == 1) {
+            if (2147483647 <= dontes1 + dontes2 or -2147483639 >= dontes1 + dontes2) {
+                inv();
+            }
+            else
+            {
+                answ = dontes1 + dontes2;
+            }
+        }
+
+        else if (dontes == 2) {
+            if (2147483647 <= dontes1 - dontes2 or -2147483639 >= dontes1 - dontes2) {
+                inv();
+            }
+            else
+            {
+                answ = dontes1 - dontes2;
+            }
+        }
+        else if (dontes == 3) {
+            if (2147483647 <= dontes1 / dontes2 or -2147483639 >= dontes1 / dontes2) {
+                inv();
+            }
+            else
+            {
+                answ = dontes1 / dontes2;
+            }
+        }
+        else if (dontes == 4) {
+            if (2147483647 <= dontes1 * dontes2 or -2147483639 >= dontes1 * dontes2) {
+                inv();
+            }
+            else
+            {
+                answ = dontes1 * dontes2;
+            }
+        }
         std::cout << "Answer : " << answ;
 
         std::string rest;
@@ -72,10 +88,12 @@ int main()
         else if (rest == "N" or rest == "n") {
             resti = false;
         }
-        else
+        else 
+        {
         std::cout << "\nYou have entered a wrong answer!\n\nrestarting in 3 seconds. . . .";
-        Sleep(3);
+        Sleep(3000);
         continue;
+        }
         
 }
     std::cout << "FINISH";
